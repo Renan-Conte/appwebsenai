c859d9d9ea02b1bd00b1272e7ab5ff4f482924d0
Renan-Conte <138166839+Renan-Conte@users.noreply.github.com>
2023-08-20 14:40:18 -0300

    Add files via upload

---
+++ appwebsenai/appweb/src/main/java/com/example/appweb/Person.java
@@ -0,0 +1,33 @@
+package com.example.appweb;
+
+public class Person {
+
+    private  String name;
+    private String genero;
+
+     private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
